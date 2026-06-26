6fa296b2517e1e5ed8a5cf0d7210bea6b99a25b4
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-27 02:11:03 +0530

    Add Fibonacci series program in Python

---
+++ Loops/Fibonacci_Series.py
@@ -0,0 +1,13 @@
+# Write a Python program to print the Fibonacci series.
+
+
+n = int(input("Enter the number of terms: "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
