a840fef154a08d6b84449a80fe359257f017af38
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-27 01:27:35 +0530

    Create Print_Natural_Numbers.py

---
+++ Loops/Print_Natural_Numbers.py
@@ -0,0 +1,9 @@
+# Write a Python program to print natural numbers from 1 to N using a for loop.
+
+
+n = int(input("Enter the value of N: "))
+
+print("Natural Numbers:")
+
+for i in range(1, n + 1):
+    print(i)
