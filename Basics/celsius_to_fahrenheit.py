6068edb522caa3e003856f9fe0bca8ca1c2e72d2
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-26 17:08:49 +0530

    Add Celsius to Fahrenheit conversion program
    This program prompts the user to enter a temperature in Celsius and converts it to Fahrenheit.

---
+++ Basics/celsius_to_fahrenheit.py
@@ -0,0 +1,8 @@
+# Write a Python program to convert Celsius into Fahrenheit.
+
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit =", fahrenheit)
