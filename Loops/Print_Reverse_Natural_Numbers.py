1fdb584f8370a8451a645e844077cc7d250cf0f4
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-27 01:28:43 +0530

    Add program to print reverse natural numbers
    This program prompts the user for a value N and prints natural numbers in reverse order from N to 1.

---
+++ Loops/Print_Reverse_Natural_Numbers.py
@@ -0,0 +1,8 @@
+Write a Python program to print natural numbers from N to 1.
+
+n = int(input("Enter the value of N: "))
+
+print("Reverse Natural Numbers:")
+
+for i in range(n, 0, -1):
+    print(i)
