e258e01b36e5b512e01aae5c391908f2868eed8c
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-29 12:14:42 +0530

    Create String_Concatenation.py

---
+++ String/String_Concatenation.py
@@ -0,0 +1,9 @@
+// Write a Python program to concatenate two strings.
+
+
+first = input("Enter first string: ")
+second = input("Enter second string: ")
+
+result = first + " " + second
+
+print("Concatenated String =", result)
