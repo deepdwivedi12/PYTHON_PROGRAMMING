cb20c22f199aa8ec944b18b504e486ec11e7982b
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-29 12:17:57 +0530

    Create Palindrome_String.py

---
+++ String/Palindrome_String.py
@@ -0,0 +1,8 @@
+// Write a Python program to check whether a string is a palindrome.
+
+text = input("Enter a string: ")
+
+if text == text[::-1]:
+    print("Palindrome String")
+else:
+    print("Not a Palindrome String")
