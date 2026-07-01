76c4dcf3e58bdcfb8749a9cbb14eea852fd75537
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-01 13:22:26 +0530

    Create Access_List_Elements.py

---
+++ Lists/Access_List_Elements.py
@@ -0,0 +1,7 @@
+// Write a Python program to access list elements using index.
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("First Element =", fruits[0])
+print("Second Element =", fruits[1])
+print("Last Element =", fruits[-1])
