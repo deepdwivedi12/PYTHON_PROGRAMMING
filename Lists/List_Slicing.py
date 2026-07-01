74cc1853cac546d44993c575385cfa27e51e01a9
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-01 13:31:54 +0530

    Create List_Slicing.py

---
+++ Lists/List_Slicing.py
@@ -0,0 +1,7 @@
+// Write a Python program to perform list slicing.
+
+numbers = [10, 20, 30, 40, 50, 60]
+
+print("Original List =", numbers)
+print("First Three Elements =", numbers[:3])
+print("Last Three Elements =", numbers[-3:])
