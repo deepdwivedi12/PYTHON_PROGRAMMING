0c078bcf54380d2ed44816a5dcfd029368230e55
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-01 13:30:03 +0530

    Create Average_of_List.py

---
+++ Lists/Average_of_List.py
@@ -0,0 +1,8 @@
+// Write a Python program to find the average of all elements in a list.
+
+numbers = [10, 20, 30, 40, 50]
+
+average = sum(numbers) / len(numbers)
+
+print("List =", numbers)
+print("Average =", average)
