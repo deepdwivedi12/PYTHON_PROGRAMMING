d917bdc23d8a658975916a4287b5c5f8aaf0e9de
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-03 14:25:21 +0530

    Create Slice_Tuple.py

---
+++ Tuples/Slice_Tuple.py
@@ -0,0 +1,7 @@
+// Write a Python program to perform tuple slicing.
+
+numbers = (10, 20, 30, 40, 50, 60)
+
+print("Original Tuple =", numbers)
+print("First Three Elements =", numbers[:3])
+print("Last Three Elements =", numbers[-3:])
