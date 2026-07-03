1e929a1719b46ce93261cec96ca7ffe139d0e45d
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-03 14:15:33 +0530

    Create Access_Tuple_Elements.py

---
+++ Tuples/Access_Tuple_Elements.py
@@ -0,0 +1,7 @@
+// Write a Python program to access tuple elements using index.
+
+fruits = ("Apple", "Banana", "Mango", "Orange")
+
+print("First Element =", fruits[0])
+print("Second Element =", fruits[1])
+print("Last Element =", fruits[-1])
