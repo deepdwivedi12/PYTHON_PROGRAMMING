f0ba9010591fd11c4218d317f034e4e51bd911cc
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 14:09:00 +0530

    Add discount eligibility check based on age and purchase

---
+++ Conditional Statemen/discount-eligibility.py
@@ -0,0 +1,6 @@
+Age = int(input("Enter your age :"))
+purchase = int(input("Enter your purchase : "))
+if(Age > 60  or purchase > 100): 
+  print("You are eligible for 20% discount")
+else:
+  print("you are not eligible for discount")
