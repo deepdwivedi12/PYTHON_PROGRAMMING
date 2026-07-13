fc1a6e020b3337535fcbba63eed8ffff4557ac85
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:25:54 +0530

    Add eligibility check for extra credit

---
+++ Conditional_Statement/Extra_credit_eligibility.py
@@ -0,0 +1,5 @@
+a = int(input("Enter criteria :"))
+if((a % 2 == 0 and a % 3 == 0) or (a >= 5 ) and (a != 9 )):
+    print("you are eligible for extra credit ")
+else:
+   print("you are not eligible for extra credit")
