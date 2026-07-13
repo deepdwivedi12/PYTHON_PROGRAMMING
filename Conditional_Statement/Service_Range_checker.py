56a644cb0e8a3e9d443c31e7450e2774cc0f2b41
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:23:24 +0530

    Correct range condition in Service_Range_checker.py
    Fix conditional statement for range checking.

---
+++ Conditional_Statement/Service_Range_checker.py
@@ -0,0 +1,6 @@
+saving = int(input("Enter your savings"))
+# if(saving >= 10 and saving <= 100 ):
+if(10 <= saving >=100):
+  print("Your saving within the target range")
+else:
+    print("you are not in the track")
