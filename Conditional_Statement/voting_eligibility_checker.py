ea4e79a7d708e00c0171c383d5ab4a9a85fedf8f
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:30:39 +0530

    Add voting eligibility checker script

---
+++ Conditional_Statement/voting_eligibility_checker.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your Age: "))
+id = input("Enter your ID: ")
+
+if(a >= 18 and id == "yes"):
+    print("You qualify to vote.")
+else:
+    print("You do not qualify to vote.")
+    
